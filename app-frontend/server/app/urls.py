def52a6008b6fc2d58ef31fb80abcd3564ddbf09
윤진 <jinjjij0226@gmail.com>
2023-10-13 15:26:33 +0900

    ㅇㅇ

--- app-frontend/server/app/urls.py
+++ app-frontend/server/app/urls.py
@@ -5,4 +5,7 @@ from .views import indexView
 urlpatterns = [
     path('', indexView.as_view() , name="index"),
     path('image/<int:id>', views.image, name="image"),
+    path('is_machine_ready', views.machine_ready, name="is_machine_ready"),
+    path('ready_from_backend', views.ready_from_backend, name="ready_from_backend"),
+    path('page_loaded', views.pageLoaded, name="page_loaded"),
 ]
